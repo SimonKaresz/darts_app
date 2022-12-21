b9b9ae83ac41a956c9bc79aa876e24cbe7b7e72b
SimonKaresz <kareszkaresz0706@gmail.com>
2022-12-21 16:40:19 +0100

    darts_app v0.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { MainMenu } from "./pages/MainMenu";
 import { Suspense } from "react";
 import { Loading } from "./components/Loading";
